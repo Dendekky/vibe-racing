f37beae7dcaa64cd4e80c9bc59a73684bc2656f9
DESKTOP-TFNDFB9\adeni <adeniyiib360@gmail.com>
2025-03-24 18:58:41 +0100

    Refactor GameCanvas import path to use the new directory structure under components/game.

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,4 +1,4 @@
-export { default as GameCanvas } from './GameCanvas';
+export { default as GameCanvas } from './components/game/GameCanvas';
 export * from './lib/physics';
 export * from './lib/car';
 export * from './lib/controls'; 
